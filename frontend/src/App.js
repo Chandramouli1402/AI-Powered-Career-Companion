a0a86e3def4c942476c7943b0309cfe14cbab3e8
Chandramouli M <chandramoulee1402@gmail.com>
2025-02-18 00:56:01 +0530

    Completed Profile Page and added a navigation button in dashboard to profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import Profile from "./components/Profile/Profile"; // Update path based on your structure
+import Dashboard from "./components/Dashboard"; // Import the Dashboard component
 import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
 import './App.css'; // Keep your global CSS file
 
@@ -15,11 +16,12 @@ function App() {
             <h1>Equipping Graduates</h1>
           </header>
 
-          {/* Define Routes for SignUp, Login, and Profile components */}
+          {/* Define Routes for SignUp, Login, Profile, and Dashboard components */}
           <Routes>
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/dashboard" element={<Dashboard />} /> {/* Add route for Dashboard */}
           </Routes>
         </div>
       </Router>
